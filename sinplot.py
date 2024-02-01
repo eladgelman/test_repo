e7129cd99bdac6dada177b0595f16f33d80a9cbf
Elad Gelman <eladgelman2@gmail.com>
2024-02-01 17:58:53 +0200

    first commit

---
+++ sinplot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,10,100)
+y = 4 + 2*np.sin(2* x)
+
+#plot
+fig, ax = plt.subplots()
+ax.plot(x, y, linewidth = 2.0)
+ax.set( xlim = (0, 8), xticks = np.arange(1,8),
+        ylim = (0, 8), yticks = np.arange(1,8))
+plt.grid()
+plt.title(" y = 4 + 2sin(2x) ")
+plt.show(block=True)
+
